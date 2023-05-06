2cd7724deb76242b94542c40085f9841f3ac49fc
unknown <damian.terefenko@gmail.com>
2023-05-06 12:10:17 +0200

    nodejs kolejny

--- App.js
+++ App.js
@@ -8,10 +8,10 @@ export default function App() {
       <View style= {styles.taskswrapper}>
       <Text style={styles.sectionTitle}>Tytul Taska</Text>
       <View style={styles.items}>
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
+      <Task text="Task 1"/>
+      <Task text="Task 2"/>
+      <Task text="Task 3"/>
+      <Task />
       </View>
       </View>
     </View>
--- components/Task.js
+++ components/Task.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-const Task =()=>{
+
+const Task =(props)=>{
     return (
-        <View>
-            <Text>Task</Text>
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>
+                <View styles={styles.square}></View>
+                <View styles={styles.itemText}>{props.text}</View>
+
+            </View>
+            <View style={styles.circular}></View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
 
+    item:{
+        backgroundColor:'#fff',
+        padding:15,
+        borderRadius:10,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        marginBottom:20,
+
+    },
+    itemLeft:{
+        flexDirection:'row',
+        alignItems:'center',
+        flexWrap:'wrap',
+    },
+    square:{ 
+        width:24,
+        height:24,
+        borderColor:'#55BCF6',
+        opacity:0.4,
+        borderRadius:5,
+        borderWidth:2,
+        marginRight:15,
+    },
+    circular:{
+        width:12,
+        height:12,
+        borderColor:'#55BCF6',
+        borderWidth:2,
+        borderRadius:5,
+    }
 })
 
 export default Task;
